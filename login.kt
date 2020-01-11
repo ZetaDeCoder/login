830c5055f926c48a100848097d7c865cd9c72f76
RAKTIM CHATTERJEE <41070396+raktim2018@users.noreply.github.com>
2020-01-11 22:37:03 +0530

    Create login.kt

---
+++ login.kt
@@ -0,0 +1,40 @@
+package com.tutorialkart.myapplication
+ 
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+ 
+/**
+* A Login Form Example in Kotlin Android
+*/
+class MainActivity : AppCompatActivity() {
+ 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+ 
+        // get reference to all views
+        var et_user_name = findViewById(R.id.et_user_name) as EditText
+        var et_password = findViewById(R.id.et_password) as EditText
+        var btn_reset = findViewById(R.id.btn_reset) as Button
+        var btn_submit = findViewById(R.id.btn_submit) as Button
+ 
+        btn_reset.setOnClickListener {
+            // clearing user_name and password edit text views on reset button click
+            et_user_name.setText("")
+            et_password.setText("")
+        }
+ 
+        // set on-click listener
+        btn_submit.setOnClickListener {
+            val user_name = et_user_name.text;
+            val password = et_password.text;
+            Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show()
+ 
+            // your code to validate the user_name and password combination
+            // and verify the same
+ 
+        }
+    }
+}
